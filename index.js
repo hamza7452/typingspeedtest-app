4b3386c117b835d85d9abf210602617e08bfe326
hamza khan <mhkhamza989@gmail.com>
2025-07-13 18:35:11 +0530

    more changes to javascript

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const showsentence = document.querySelector('#show-sentence');
 const typingspace = document.querySelector('#textarea');
 const button = document.querySelector('#btn');
-const score = document.querySelector('#score');
+const score = document.querySelector('.score');
 
 let starttime, endtime, totaltimetaken;
 const sentences = ["In a world full of distractions, staying focused is the real superpower.",
@@ -10,14 +10,37 @@ const sentences = ["In a world full of distractions, staying focused is the real
   "While some people dream of success, others wake up and work hard for it.",
   "JavaScript is a powerful language that can make websites interactive and dynamic."]
 
+function counttypingspeed(timetaken){
+  let totalwords = typingspace.value.trim();
+let actualwords = totalwords === '' ? 0 : totalwords.split(/\s+/).length;
+
+  if (actualwords !== 0) {
+    let Typingspeed = Math.round((actualwords / timetaken) * 60);
+    score.innerHTML = `your typing speed is ${Typingspeed} words per minute and you wrote ${actualwords} words in time ${timetaken}s`;
+  }
+  else {
+    score.innerHTML=`your typing speed is 0 words per minute and time taken ${timetaken} sec`
+  }
+  }
+
 function starttyping() {
   let randomno = Math.floor(Math.random() * sentences.length);
   showsentence.innerHTML = sentences[randomno];
   let date = new Date();
-  let starttime = date.getTime();
+  starttime = date.getTime();
   button.innerHTML = "Done";
 
-  }
+}
+  
+function endtyping() {
+  button.innerHTML = "Start";
+  let date = new Date();
+  endtime = date.getTime();
+  totaltimetaken = (endtime - starttime) / 1000;
+  counttypingspeed(totaltimetaken);
+  showsentence.innerHTML = "";
+  typingspace.value = "";  
+}
 
 button.addEventListener('click', () => {
   if (button.innerText.toLowerCase() === "start") {
